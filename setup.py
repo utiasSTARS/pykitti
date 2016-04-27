98184ddae62636489d7286ee6e78dbf1d238a4c9
Lee Clement <lee.clement@robotics.utias.utoronto.ca>
2016-04-27 18:27:50 -0400

    PyPI release

--- setup.py
+++ setup.py
@@ -1,28 +1,13 @@
 from setuptools import setup
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE.md') as f:
-    license = f.read()
-
-try:
-    from pypandoc import convert
-    readme = convert(readme, 'rst', format='md')
-    license = convert(license, 'rst', format='md')
-except ImportError:
-    print("Warning: pypandoc module not found, could not convert Markdown to RST")
-
 setup(
     name='pykitti',
-    version='0.1.0',
+    version='0.1.1',
     description='A minimal set of tools for working with the KITTI dataset in Python',
-    long_description=readme,
     author='Lee Clement',
     author_email='lee.clement@robotics.utias.utoronto.ca',
     url='https://github.com/utiasSTARS/pykitti',
-    download_url='https://github.com/utiasSTARS/pykitti/tarball/0.1.0',
-    license=license,
+    download_url='https://github.com/utiasSTARS/pykitti/tarball/0.1.1',
     packages=['pykitti'],
     install_requires=['numpy', 'matplotlib']
 )
