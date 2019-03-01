47425f5d068de954e10d11d2959966917bee24ff
Lee Clement <clement.leopold@gmail.com>
2019-02-28 20:39:19 -0500

    Import tracking module

--- pykitti/__init__.py
+++ pykitti/__init__.py
@@ -2,6 +2,7 @@
 
 from pykitti.odometry import odometry
 from pykitti.raw import raw
+from pykitti.tracking import tracking
 
 __author__ = "Lee Clement"
 __email__ = "lee.clement@robotics.utias.utoronto.ca"
